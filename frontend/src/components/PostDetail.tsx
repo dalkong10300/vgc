7299d52041376b864b9da0c94c4843a1c1a55d9c
doolkii <dalkong10300@gmail.com>
2026-02-19 17:22:46 +0900

    prd s3 업로드

--- frontend/src/components/PostDetail.tsx
+++ frontend/src/components/PostDetail.tsx
@@ -22,7 +22,6 @@ const colorClassMap: Record<string, string> = {
   red: "bg-red-100 text-red-800",
   pink: "bg-pink-100 text-pink-800",
   indigo: "bg-indigo-100 text-indigo-800",
-  orange: "bg-orange-100 text-orange-800",
   gray: "bg-gray-100 text-gray-800",
 };
 
